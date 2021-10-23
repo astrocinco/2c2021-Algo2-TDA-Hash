7f01e19d9d94c807332d9c44f0d090b53e79a1d0
astrocinco <santiagolps3@gmail.com>
2021-10-23 18:14:41 -0300

    esqueleto

--- hash.c
+++ hash.c
@@ -2,20 +2,65 @@
 // Hay que definir si hacemos un hash abierto o cerrado. Y qué tipo, si elegimos cerrado
 // Supongo que la decisión dependerá de que tan bueno y dificil lo querramos hacer
 
-typedef struct hash hash_t;
-typedef struct hash_iter hash_iter_t;
-typedef void (*hash_destruir_dato_t)(void *);
+// ESTRUCTURAS
+typedef struct hash {
 
-hash_t *hash_crear(hash_destruir_dato_t destruir_dato);
-bool hash_guardar(hash_t *hash, const char *clave, void *dato);
-void *hash_borrar(hash_t *hash, const char *clave);
-void *hash_obtener(const hash_t *hash, const char *clave);
-bool hash_pertenece(const hash_t *hash, const char *clave);
-size_t hash_cantidad(const hash_t *hash);
-void hash_destruir(hash_t *hash);
+} hash_t;
 
-hash_iter_t *hash_iter_crear(const hash_t *hash);
-bool hash_iter_avanzar(hash_iter_t *iter);
-const char *hash_iter_ver_actual(const hash_iter_t *iter);
-bool hash_iter_al_final(const hash_iter_t *iter);
-void hash_iter_destruir(hash_iter_t* iter);
+typedef struct hash_iter {
+
+} hash_iter_t;
+
+typedef void (*hash_destruir_dato_t)(void *){
+
+} // QUE CARARJO ES ESTO ------------------
+
+// PRIMITIVAS HASH
+hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
+
+}
+
+bool hash_guardar(hash_t *hash, const char *clave, void *dato){
+
+}
+
+void *hash_borrar(hash_t *hash, const char *clave){
+
+}
+
+void *hash_obtener(const hash_t *hash, const char *clave){
+
+}
+
+bool hash_pertenece(const hash_t *hash, const char *clave){
+
+}
+
+size_t hash_cantidad(const hash_t *hash){
+
+}
+
+void hash_destruir(hash_t *hash){
+
+}
+
+// PRIMITIVAS ITERADOR
+hash_iter_t *hash_iter_crear(const hash_t *hash){
+
+}
+
+bool hash_iter_avanzar(hash_iter_t *iter){
+
+}
+
+const char *hash_iter_ver_actual(const hash_iter_t *iter){
+
+}
+
+bool hash_iter_al_final(const hash_iter_t *iter){
+
+}
+
+void hash_iter_destruir(hash_iter_t* iter){
+
+}
