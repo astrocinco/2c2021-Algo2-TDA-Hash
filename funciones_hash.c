8403eefa6d9f952060e34e2680f9acd1aaf6e717
astrocinco <santiagolps3@gmail.com>
2021-10-25 01:06:18 -0300

    .

--- funciones_hash.c
+++ funciones_hash.c
@@ -1,1 +1,6 @@
-// AQUI TODAS LAS FUNCIONES DE HASH QUE USEMOS
+// AQUI TODAS LAS FUNCIONES DE HASH QUE PODRIAMOS USAR
+// https://softwareengineering.stackexchange.com/questions/49550/which-hashing-algorithm-is-best-for-uniqueness-and-speed
+// http://www.cse.yorku.ca/~oz/hash.html
+// https://stackoverflow.com/questions/34595/what-is-a-good-hash-function
+// http://burtleburtle.net/bob/hash/doobs.html
+// 
