ac637dc9891e484221f3e4497c181b1effd9a0a1
astrocinco <santiagolps3@gmail.com>
2021-10-26 13:46:36 -0300

    v1.4

--- hash.c
+++ hash.c
@@ -8,22 +8,22 @@
 #define FACTOR_NVO_TAM 10
 #define MAX_FACTOR_CARGA 0.8
 #define MIN_FACTOR_CARGA 0.1
-#define FUN_HASHING1 djb2 // NO SE SI ESTO FUNCIONA. VER hash_encontrar_pos_libre()
-#define FUN_HASHING2 sdbm // NO SE SI ESTO FUNCIONA. VER hash_encontrar_pos_libre()
+#define FUN_HASHING1 djb2 
+#define FUN_HASHING2 sdbm 
 #define FUN_HASHING3 XXX // ELEGIR FUNCION DE HASHING
 #define FUN_HASHING4 XXX // ELEGIR FUNCION DE HASHING
 
 // ----ESTRUCTURAS----
 
 typedef struct hash {
-    void* lista;
+    void** lista;
     int capacidad_lista;
-    int cantidad_en_lista;
+    int cantidad_lista;
 } hash_t;
 
 typedef struct campo {
     char* clave;
-    void* contenido;
+    void* dato;
 } campo_t;
 
 typedef struct hash_iter {
@@ -37,7 +37,10 @@ typedef void (*hash_destruir_dato_t)(void *){
 // ----PRIMITIVAS----
 
 // PRIMITIVAS CAMPO
-campo_t* crear_campo(char* clave, void* dato){
+campo_t* crear_campo(const char* clave, void* dato){
+    campo_t* campo = malloc(sizeof(campo));
+    if (campo == NULL) return NULL;
+
     campo->clave = clave;
     campo->dato = dato;
     return campo;
@@ -54,8 +57,8 @@ hash_t *hash_crear(){
     if (hash == NULL) return NULL;
 
     hash->lista = malloc(sizeof(campo_t) * CAPACIDAD_INICIAL);
-    hash->cantidad_en_lista = 0;
-    hash->capacidad_lista =  = CAPACIDAD_INICIAL;
+    hash->cantidad_lista = 0;
+    hash->capacidad_lista = CAPACIDAD_INICIAL;
 
     return hash;
 }
@@ -68,17 +71,17 @@ bool pos_esta_ocupada(hash_t *hash, int posicion){
     return (hash->lista[posicion] != NULL);
 }
 
-bool hash_guardar_aux(hash_t* hash, int posicion, char* clave, void* dato){
-    campo_t* nuevo_campo = crear_campo(char* clave, void* dato);
-    if (nuevo_campo == NULL) return false
+bool hash_guardar_aux(hash_t* hash, int posicion, const char* clave, void* dato){
+    campo_t* nuevo_campo = crear_campo(clave, dato);
+    if (nuevo_campo == NULL) return false;
 
     (hash->lista)[posicion] = nuevo_campo;
-    (hash->cantidad_en_lista)++;
+    (hash->cantidad_lista)++;
     return true;
 }
 
 bool hash_encontrar_pos_libre(hash_t *hash, const char *clave, void *dato){
-    if (cantidad_en_lista / capacidad_en_lista > MAX_FACTOR_CARGA) hash_redimensionar(hash, hash->capacidad_lista * FACTOR_NVO_TAM); // CREO QUE TENGO QUE ARMAR UNA LISTA CON NUEVAS CAPACIDAD QUE SEAN NUMEROS PRIMOS, NO UN MULTIPLICADOR DE NUEVO TAMAÑO
+    if (hash->cantidad_lista / hash->capacidad_lista > MAX_FACTOR_CARGA) hash_redimensionar(hash, hash->capacidad_lista * FACTOR_NVO_TAM); // CREO QUE TENGO QUE ARMAR UNA LISTA CON NUEVAS CAPACIDAD QUE SEAN NUMEROS PRIMOS, NO UN MULTIPLICADOR DE NUEVO TAMAÑO
 
     //int pos_fun_hash = djb2(clave) % hash->capacidad_lista; // EN CASO QUE FUN_HASHING° NO FUNCIONE
     int pos_fun_hash = FUN_HASHING1(clave) % hash->capacidad_lista;
@@ -94,47 +97,6 @@ bool hash_encontrar_pos_libre(hash_t *hash, const char *clave, void *dato){
     if (!pos_esta_ocupada(hash, pos_fun_hash)) return hash_guardar_aux(hash, pos_fun_hash, clave, dato);
 
     return false; // ESTO DEBERÍA SER UN RETURN FALSE O UNA LLAMADA A REDIMENSIONAR E INTENTAR TODA LA FUNCION DE NUEVO?
-}
-
-bool hash_guardar(hash_t *hash, const char *clave, void *dato){ // FINISH
-    hash_borrar(hash, clave);
-    return hash_encontrar_pos_libre();
-}
-
-void *hash_borrar(hash_t *hash, const char *clave){
-    void* dato_resultado = hash_obtener(hash, clave);
-    if (dato == NULL) return dato;
-
-    int pos_a_eliminar = FUN_HASHING1(clave) % hash->capacidad_lista;
-    if (hash->lista[pos_a_eliminar]->dato == dato_resultado) hash->lista[pos_a_eliminar] = NULL; // Puede que este == sea incorrecto
-
-    pos_a_eliminar = FUN_HASHING2(clave) % hash->capacidad_lista;
-    if (hash->lista[pos_a_eliminar]->dato == dato_resultado) hash->lista[pos_a_eliminar] = NULL;
-
-    pos_a_eliminar = FUN_HASHING3(clave) % hash->capacidad_lista;
-    if (hash->lista[pos_a_eliminar]->dato == dato_resultado) hash->lista[pos_a_eliminar] = NULL;
-
-    pos_a_eliminar = FUN_HASHING4(clave) % hash->capacidad_lista;
-    if (hash->lista[pos_a_eliminar]->dato == dato_resultado) hash->lista[pos_a_eliminar] = NULL;
-}
-
-void *hash_obtener(const hash_t *hash, const char *clave_ingresada){
-    if (!hash_pertenece(hash, clave)) return NULL;
-
-    int hashing_clave = FUN_HASHING1(clave) % hash->capacidad_lista;
-    if (strcmp((hash->lista)[hashing_clave]->clave, clave_ingresada) == 0) return (hash->lista)[hashing_clave]->dato;
-
-    hashing_clave = FUN_HASHING2(clave) % hash->capacidad_lista;
-    if (strcmp((hash->lista)[hashing_clave]->clave, clave_ingresada) == 0) return (hash->lista)[hashing_clave]->dato;
-
-    hashing_clave = FUN_HASHING3(clave) % hash->capacidad_lista;
-    if (strcmp((hash->lista)[hashing_clave]->clave, clave_ingresada) == 0) return (hash->lista)[hashing_clave]->dato;
-
-    hashing_clave = FUN_HASHING4(clave) % hash->capacidad_lista;
-    if (strcmp((hash->lista)[hashing_clave]->clave, clave_ingresada) == 0) return (hash->lista)[hashing_clave]->dato;
-
-    printf("REVISAR HASH_OBTENER hash.c");
-    return NULL; // NUNCA DEBERÍA LLEGAR AQUI 
 }
 
 bool hash_pertenece(const hash_t *hash, const char *clave_ingresada){
@@ -161,8 +123,49 @@ bool hash_pertenece(const hash_t *hash, const char *clave_ingresada){
     return false;
 }
 
+void *hash_obtener(const hash_t *hash, const char *clave_ingresada){
+    if (!hash_pertenece(hash, clave)) return NULL;
+
+    int hashing_clave = FUN_HASHING1(clave) % hash->capacidad_lista;
+    if (strcmp((hash->lista)[hashing_clave]->clave, clave_ingresada) == 0) return (hash->lista)[hashing_clave]->dato;
+
+    hashing_clave = FUN_HASHING2(clave) % hash->capacidad_lista;
+    if (strcmp((hash->lista)[hashing_clave]->clave, clave_ingresada) == 0) return (hash->lista)[hashing_clave]->dato;
+
+    hashing_clave = FUN_HASHING3(clave) % hash->capacidad_lista;
+    if (strcmp((hash->lista)[hashing_clave]->clave, clave_ingresada) == 0) return (hash->lista)[hashing_clave]->dato;
+
+    hashing_clave = FUN_HASHING4(clave) % hash->capacidad_lista;
+    if (strcmp((hash->lista)[hashing_clave]->clave, clave_ingresada) == 0) return (hash->lista)[hashing_clave]->dato;
+
+    printf("REVISAR HASH_OBTENER hash.c");
+    return NULL; // NUNCA DEBERÍA LLEGAR AQUI 
+}
+
+void *hash_borrar(hash_t *hash, const char *clave){
+    void* dato_resultado = hash_obtener(hash, clave);
+    if (dato_resultado == NULL) return dato_resultado;
+
+    int pos_a_eliminar = FUN_HASHING1(clave) % hash->capacidad_lista;
+    if (hash->lista[pos_a_eliminar]->dato == dato_resultado) hash->lista[pos_a_eliminar] = NULL; // Puede que este == sea incorrecto
+
+    pos_a_eliminar = FUN_HASHING2(clave) % hash->capacidad_lista;
+    if (hash->lista[pos_a_eliminar]->dato == dato_resultado) hash->lista[pos_a_eliminar] = NULL;
+
+    pos_a_eliminar = FUN_HASHING3(clave) % hash->capacidad_lista;
+    if (hash->lista[pos_a_eliminar]->dato == dato_resultado) hash->lista[pos_a_eliminar] = NULL;
+
+    pos_a_eliminar = FUN_HASHING4(clave) % hash->capacidad_lista;
+    if (hash->lista[pos_a_eliminar]->dato == dato_resultado) hash->lista[pos_a_eliminar] = NULL;
+}
+
+bool hash_guardar(hash_t *hash, const char *clave, void *dato){ // FINISH
+    hash_borrar(hash, clave);
+    return hash_encontrar_pos_libre(hash, clave, dato);
+}
+
 size_t hash_cantidad(const hash_t *hash){
-    return hash->cantidad_en_lista;
+    return hash->cantidad_lista;
 }
 
 void hash_destruir(hash_t *hash){
