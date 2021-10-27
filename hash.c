9bbb2aafcebe63f4c335fb9acd5ef4dcd4b854f6
astrocinco <santiagolps3@gmail.com>
2021-10-27 02:46:29 -0300

    V1.8

--- hash.c
+++ hash.c
@@ -40,23 +40,23 @@ typedef void (*hash_destruir_dato_t)(void *){
 hash_iter_t *hash_iter_crear(const hash_t *hash){
     hash_iter_t* iterador = malloc(sizeof(hash_iter_t));
     int altura_en_lista = 0;
-
+    // TERMINAR
 }
 
 bool hash_iter_avanzar(hash_iter_t *iter){
-
+    // TO DO
 }
 
 const char *hash_iter_ver_actual(const hash_iter_t *iter){
-
+    // TO DO
 }
 
 bool hash_iter_al_final(const hash_iter_t *iter){
-
+    // TO DO
 }
 
 void hash_iter_destruir(hash_iter_t* iter){
-
+    // TO DO
 }
 
 // PRIMITIVAS CAMPO
@@ -270,4 +270,3 @@ void hash_destruir(hash_t *hash){
     free(hash->lista);
     free(hash);
 }
-
